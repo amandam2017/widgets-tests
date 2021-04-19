a3656b36e4d968663162b1d0df433687d4a4864d
Amanda <amandamaarman19911@gmail.com>
2021-04-19 10:26:35 +0200

    resting calculateBill test

--- factory-functions-tests/calculate-bill-test.js
+++ factory-functions-tests/calculate-bill-test.js
@@ -1,9 +1,21 @@
 describe("test calculateBill function using the available cost for billItems(sms and calls)", function(){
-    it("take a string call/sms and return the cost of call/sms", function(){
-     //   let calculateBill = billWithString();
+    it("take a string call/sms and return the total cost of call/sms", function(){
 
-        assert.equal(totalPhoneBill("sms,sms,sms"),2.25);
-      
+        assert.equal(totalPhoneBill("sms,sms,sms,call,call,call"),10.50);  
 
     });
+
+    it("take a string sms and should return the total cost of sms ", function(){
+        //   let calculateBill = billWithString();
+           var smsStringTotal = totalPhoneBill("sms,sms,sms,sms,sms")
+   
+           assert.equal(smsStringTotal,3.75);
+       });
+
+       it("take a string call and should return the total cost of call ", function(){
+        //   let calculateBill = billWithString();
+           var callStringTotal = totalPhoneBill("call,call,call,call")
+   
+           assert.equal(11.00,(callStringTotal));
+       });
 } )
